3f9a28ef813f44e087ce1102f2abb93f696c7aaf
fanker <205226@stu.hebut.edu.cn>
2022-09-26 20:55:29 +0800

    first

---
+++ src/main/java/tut5/Calc.java
@@ -0,0 +1,14 @@
+package tut5;
+
+public class Calc {
+    public static int Add(){
+        int a=0;
+        int b=0;
+        return a+b;
+    }
+    public static int subtract(){
+        int a=0;
+        int b=0;
+        return a+b;
+    }
+}
---
+++ src/test/java/tut5/CalcTest.java
@@ -0,0 +1,13 @@
+package tut5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @Test
+    void add() {
+        assertEquals(0,new Calc().Add());
+    }
+}
